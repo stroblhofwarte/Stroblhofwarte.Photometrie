a93387eb6b1f44b47093341afd4b3103345c409c
Othmar Ehrhardt <65812096+stroblhofwarte@users.noreply.github.com>
2023-05-01 11:18:17 +0200

    Transformation Coeff: Linear regression for Tx_yz

---
+++ Stroblhofwarte.AperturePhotometry/LinearRegression.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroblhofwarte.AperturePhotometry
+{
+    public class LinearRegression
+    {
+        // Code from: VISHMAY, StackOverflow
+        public static void FitLinear(double[] xVals, double[] yVals,
+                                        int inclusiveStart, int exclusiveEnd,
+                                        out double rsquared, out double yintercept,
+                                        out double slope)
+        {
+            double sumOfX = 0;
+            double sumOfY = 0;
+            double sumOfXSq = 0;
+            double sumOfYSq = 0;
+            double ssX = 0;
+            double ssY = 0;
+            double sumCodeviates = 0;
+            double sCo = 0;
+            double count = exclusiveEnd - inclusiveStart;
+
+            for (int ctr = inclusiveStart; ctr < exclusiveEnd; ctr++)
+            {
+                double x = xVals[ctr];
+                double y = yVals[ctr];
+                sumCodeviates += x * y;
+                sumOfX += x;
+                sumOfY += y;
+                sumOfXSq += x * x;
+                sumOfYSq += y * y;
+            }
+            ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
+            ssY = sumOfYSq - ((sumOfY * sumOfY) / count);
+            double RNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
+            double RDenom = (count * sumOfXSq - (sumOfX * sumOfX))
+             * (count * sumOfYSq - (sumOfY * sumOfY));
+            sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
+
+            double meanX = sumOfX / count;
+            double meanY = sumOfY / count;
+            double dblR = RNumerator / Math.Sqrt(RDenom);
+            rsquared = dblR * dblR;
+            yintercept = meanY - ((sCo / ssX) * meanX);
+            slope = sCo / ssX;
+        }
+    }
+}
--- Stroblhofwarte.Photometrie/ViewModel/TransformationViewModel.cs
+++ Stroblhofwarte.Photometrie/ViewModel/TransformationViewModel.cs
@@ -1,6 +1,8 @@
-﻿using OxyPlot;
+﻿using ControlzEx.Standard;
+using OxyPlot;
 using OxyPlot.Series;
 using Stroblhofwarte.AperturePhotometry;
+using Stroblhofwarte.Photometrie.FileFormats;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,14 +12,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace Stroblhofwarte.Photometrie.ViewModel
 {
     public class TransformationViewModel : DockWindowViewModel
     {
+        #region Constant
+
+        private double _MACHINE_MAG_C = 20.0;
+
+        #endregion
+
         #region Properties
 
         private ScatterSeries _tx_yz_series;
+        private LineSeries _tx_yz_fit;
 
         private ObservableCollection<TrandformationEntry> _data;
         public ObservableCollection<TrandformationEntry> Data
@@ -47,6 +57,52 @@ namespace Stroblhofwarte.Photometrie.ViewModel
         }
 
         #endregion
+
+        #region Commands
+
+        private RelayCommand commandDel;
+        public ICommand CommandDel
+        {
+            get
+            {
+                if (commandDel == null)
+                {
+                    commandDel = new RelayCommand(param => this.Del(param), param => this.CanDel());
+                }
+                return commandDel;
+            }
+        }
+
+        private bool CanDel()
+        {
+            return true;
+        }
+
+        private void Del(object o)
+        {
+            TrandformationEntry t = o as TrandformationEntry;
+            if (t == null) return;
+            int idx = -1;
+            int i = 0;
+            foreach(TrandformationEntry te in _data)
+            {
+                if(te.Ident == t.Ident)
+                {
+                    idx = i;
+                    break;
+                }
+                i++;
+            }
+            if(idx != -1)
+            {
+                _data.RemoveAt(idx);
+            }
+            UpdateTx_yz();
+        }
+
+
+        #endregion
+
         #region Ctor
         public TransformationViewModel()
         {
@@ -62,9 +118,10 @@ namespace Stroblhofwarte.Photometrie.ViewModel
 
             Model_Tx_yz = new PlotModel { Title = "Tx_yz", Subtitle = "(Magnitude Transformation)"};
 
-            _tx_yz_series = new ScatterSeries { Title = "Series 1", MarkerType = MarkerType.Circle };
+            _tx_yz_series = new ScatterSeries { Title = "Magnitudes", MarkerType = MarkerType.Circle };
+            _tx_yz_fit = new LineSeries { Title = "Transform slop", MarkerType = MarkerType.Circle };
             Model_Tx_yz.Series.Add(_tx_yz_series);
-            
+            Model_Tx_yz.Series.Add(_tx_yz_fit);
         }
 
         private void Instance_NewMeasurement(object? sender, EventArgs e)
@@ -78,12 +135,45 @@ namespace Stroblhofwarte.Photometrie.ViewModel
                 VR = TransformDataSet.Instance.VR,
                 RI = TransformDataSet.Instance.RI
             });
-            _tx_yz_series.Points.Add(new ScatterPoint(TransformDataSet.Instance.BV, TransformDataSet.Instance.Mag + 20 - TransformDataSet.Instance.V));
-            Model_Tx_yz.InvalidatePlot(true);
+            UpdateTx_yz();
         }
         #endregion
+        private void UpdateTx_yz()
+        {
+            _tx_yz_series.Points.Clear();
+            foreach (TrandformationEntry t in _data)
+            {
+                ScatterPoint p = new ScatterPoint(t.BV, t.Meas + _MACHINE_MAG_C - t.V);
+                _tx_yz_series.Points.Add(p);
+            }
+
+            if (_data.Count > 5)
+            {
+                double[] x = new double[_data.Count];
+                double[] y = new double[_data.Count];
+                int idx = 0;
+                double xmax = double.MinValue;
+                foreach (TrandformationEntry d in _data)
+                {
+                    x[idx] = d.BV;
+                    y[idx++] = d.Meas + _MACHINE_MAG_C - d.V;
+                    if (d.BV > xmax)
+                        xmax = d.BV;
+                }
+                double rSuqared = 0.0;
+                double yIntersect = 0.0;
+                double slope = 0.0;
+                LinearRegression.FitLinear(x, y, 0, _data.Count, out rSuqared, out yIntersect, out slope);
+                _tx_yz_fit.Points.Clear();
+                _tx_yz_fit.Points.Add(new DataPoint(0, yIntersect));
+                _tx_yz_fit.Points.Add(new DataPoint(xmax, yIntersect + (slope * xmax)));
+
+            }
+            Model_Tx_yz.InvalidatePlot(true);
+        }
     }
 
+    
     public class TransformModel
     {
         public string FromFilter { get; set; }
@@ -147,6 +237,23 @@ namespace Stroblhofwarte.Photometrie.ViewModel
             get { return _RI; }
             set { _RI = value; OnPropertyChanged("RI"); }
         }
+
+        private Guid _ident;
+        public Guid Ident
+        {
+            get { return _ident; }
+        }
+
+        
+
+        #region Ctor
+
+        public TrandformationEntry()
+        {
+            _ident = Guid.NewGuid();
+        }
+
+        #endregion
 
         public event PropertyChangedEventHandler PropertyChanged;
 
