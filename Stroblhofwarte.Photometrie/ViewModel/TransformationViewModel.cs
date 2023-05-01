83cebce7157b5931b3bbbce0e539a49fca9e521b
Othmar Ehrhardt <65812096+stroblhofwarte@users.noreply.github.com>
2023-05-01 19:52:46 +0200

    Color Magnitude Error coeff Tx_yz calculated. Not stored yet.

--- Stroblhofwarte.Photometrie/ViewModel/TransformationViewModel.cs
+++ Stroblhofwarte.Photometrie/ViewModel/TransformationViewModel.cs
@@ -2,6 +2,7 @@
 using OxyPlot;
 using OxyPlot.Series;
 using Stroblhofwarte.AperturePhotometry;
+using Stroblhofwarte.Image;
 using Stroblhofwarte.Photometrie.FileFormats;
 using System;
 using System.Collections.Generic;
@@ -29,6 +30,12 @@ namespace Stroblhofwarte.Photometrie.ViewModel
         private ScatterSeries _tx_yz_series;
         private LineSeries _tx_yz_fit;
 
+        private ObservableCollection<ColorMagErrorEntry> _colorMagErrorFilters = new ObservableCollection<ColorMagErrorEntry>();
+        public ObservableCollection<ColorMagErrorEntry> ColorMagErrorFilters
+        {
+            get { return _colorMagErrorFilters; }
+        }
+
         private ObservableCollection<TrandformationEntry> _data;
         public ObservableCollection<TrandformationEntry> Data
         {
@@ -53,6 +60,30 @@ namespace Stroblhofwarte.Photometrie.ViewModel
             set { 
                 _transformModels = value;
                 OnPropertyChanged("TransfromModels");
+            }
+        }
+
+        private TransformModel _transfromModel;
+        public TransformModel TransfromModelSelection
+        {
+            get { return _transfromModel; }
+            set
+            {
+                _transfromModel = value;
+                UpdateTx_yz();
+                OnPropertyChanged("TransfromModelSelection");
+            }
+        }
+
+        private ColorMagErrorEntry _colorMagErrorFilter;
+        public ColorMagErrorEntry ColorMagErrorFilter
+        {
+            get { return _colorMagErrorFilter; }
+            set
+            {
+                _colorMagErrorFilter = value;
+                UpdateTx_yz();
+                OnPropertyChanged("ColorMagErrorFilter");
             }
         }
 
@@ -99,7 +130,37 @@ namespace Stroblhofwarte.Photometrie.ViewModel
             }
             UpdateTx_yz();
         }
+        public string MagErrorParameterName
+        {
+            get
+            {
+                string errorColor = "";
+                if (ColorMagErrorFilter.Filter == "U") errorColor = "u";
+                if (ColorMagErrorFilter.Filter == "B") errorColor = "b";
+                if (ColorMagErrorFilter.Filter == "V") errorColor = "v";
+                if (ColorMagErrorFilter.Filter == "R") errorColor = "r";
+                if (ColorMagErrorFilter.Filter == "I") errorColor = "i";
 
+                string index = "";
+                if (TransfromModelSelection.FromFilter == "U" && TransfromModelSelection.ToFilter == "B") index = "ub";
+                if (TransfromModelSelection.FromFilter == "B" && TransfromModelSelection.ToFilter == "V") index = "bv";
+                if (TransfromModelSelection.FromFilter == "V" && TransfromModelSelection.ToFilter == "R") index = "vr";
+                if (TransfromModelSelection.FromFilter == "R" && TransfromModelSelection.ToFilter == "I") index = "ri";
+
+                return "T" + errorColor + "__" + index;
+            }
+        }
+
+        private double _magErrorSlope;
+        public double MagErrorSlope
+        {
+            get { return _magErrorSlope;  }
+            set {
+                _magErrorSlope = value;
+                OnPropertyChanged("MagErrorParameterName");
+                OnPropertyChanged("MagErrorSlope");
+            }
+        }
 
         #endregion
 
@@ -110,7 +171,7 @@ namespace Stroblhofwarte.Photometrie.ViewModel
             {   new TransformModel() { FromFilter = "B", ToFilter = "V"},
                 new TransformModel() { FromFilter = "U", ToFilter = "B"},
                 new TransformModel() { FromFilter = "V", ToFilter = "R"},
-                new TransformModel() { FromFilter = "R", ToFilter = "I"},
+                new TransformModel() { FromFilter = "R", ToFilter = "I"}
             };
             _data = new ObservableCollection<TrandformationEntry>();
 
@@ -126,15 +187,29 @@ namespace Stroblhofwarte.Photometrie.ViewModel
 
         private void Instance_NewMeasurement(object? sender, EventArgs e)
         {
-            
+            string filter = Stroblhofwarte.AperturePhotometry.Filter.Instance.TranslateToAAVSOFilter(StroblhofwarteImage.Instance.GetFilter());
             _data.Add(new TrandformationEntry() { Id= TransformDataSet.Instance.Id,
                 Meas = TransformDataSet.Instance.Mag,
                 V = TransformDataSet.Instance.V,
                 BV = TransformDataSet.Instance.BV,
                 UB = TransformDataSet.Instance.UB,
                 VR = TransformDataSet.Instance.VR,
-                RI = TransformDataSet.Instance.RI
+                RI = TransformDataSet.Instance.RI,
+                Filter = filter
             });
+
+            ColorMagErrorEntry cme = new ColorMagErrorEntry() { Filter = filter };
+            if(!_colorMagErrorFilters.Contains(cme))
+            {
+                _colorMagErrorFilters.Add(cme);
+                if (_colorMagErrorFilters.Count == 1)
+                {
+                    // First add -> select this filter as color mag error
+                    ColorMagErrorFilter = cme;
+                }
+                
+                OnPropertyChanged("ColorMagErrorFilters");
+            }
             UpdateTx_yz();
         }
         #endregion
@@ -143,7 +218,10 @@ namespace Stroblhofwarte.Photometrie.ViewModel
             _tx_yz_series.Points.Clear();
             foreach (TrandformationEntry t in _data)
             {
-                ScatterPoint p = new ScatterPoint(t.BV, t.Meas + _MACHINE_MAG_C - t.V);
+                if (t.Filter != ColorMagErrorFilter.Filter) 
+                    continue;
+                double indice = ColorIndexFromData(_transfromModel, t);
+                ScatterPoint p = new ScatterPoint(indice, t.Meas + _MACHINE_MAG_C - t.V);
                 _tx_yz_series.Points.Add(p);
             }
 
@@ -155,15 +233,18 @@ namespace Stroblhofwarte.Photometrie.ViewModel
                 double xmax = double.MinValue;
                 foreach (TrandformationEntry d in _data)
                 {
-                    x[idx] = d.BV;
+                    double indice = ColorIndexFromData(_transfromModel, d);
+
+                    x[idx] = indice;
                     y[idx++] = d.Meas + _MACHINE_MAG_C - d.V;
-                    if (d.BV > xmax)
-                        xmax = d.BV;
+                    if (indice > xmax)
+                        xmax = indice;
                 }
                 double rSuqared = 0.0;
                 double yIntersect = 0.0;
                 double slope = 0.0;
                 LinearRegression.FitLinear(x, y, 0, _data.Count, out rSuqared, out yIntersect, out slope);
+                MagErrorSlope = slope;
                 _tx_yz_fit.Points.Clear();
                 _tx_yz_fit.Points.Add(new DataPoint(0, yIntersect));
                 _tx_yz_fit.Points.Add(new DataPoint(xmax, yIntersect + (slope * xmax)));
@@ -171,8 +252,18 @@ namespace Stroblhofwarte.Photometrie.ViewModel
             }
             Model_Tx_yz.InvalidatePlot(true);
         }
+        private double ColorIndexFromData(TransformModel filter, TrandformationEntry dataline)
+        {
+            double indice = 0.0;
+            if (filter.FromFilter == "U" && filter.ToFilter == "B") indice = dataline.UB;
+            if (filter.FromFilter == "B" && filter.ToFilter == "V") indice = dataline.BV;
+            if (filter.FromFilter == "V" && filter.ToFilter == "R") indice = dataline.VR;
+            if (filter.FromFilter == "R" && filter.ToFilter == "I") indice = dataline.RI;
+            return indice;
+        }
     }
 
+    
     
     public class TransformModel
     {
@@ -260,6 +351,45 @@ namespace Stroblhofwarte.Photometrie.ViewModel
         protected virtual void OnPropertyChanged(string propertyName)
         {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ColorMagErrorEntry : INotifyPropertyChanged
+    {
+        private string _filter;
+        public string Filter
+        {
+            get { return _filter; }
+            set { _filter = value; OnPropertyChanged("Filter"); }
+        }
+
+        #region Ctor
+
+
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            ColorMagErrorEntry f = obj as ColorMagErrorEntry;
+            if (f == null) return false;
+            return f.Filter == Filter;
+        }
+
+        public override int GetHashCode()
+        {
+            return Filter.GetHashCode();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return Filter;
         }
     }
 }
